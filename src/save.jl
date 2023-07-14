4db75ae86e635c654e0226d39cc233e8a4f1ad0f
Emily Gu <emilygnzsas5@gmail.com>
2023-07-13 17:04:39 -0700

    Changed documentation for save_nc

--- src/save.jl
+++ src/save.jl
@@ -11,20 +11,21 @@
 #######################################################################################################################################################################################################
 """
 
-    save_nc!(file::String, var_name::String, var_data::Array{T,N}, var_attribute::Dict{String,String}; compress::Int = 4, growable::Bool = false) where {T<:Union{AbstractFloat,Int,String},N}
+save_nc!(file::String, var_name::String, var_data::Array{T,N}, var_attribute::Dict{String,String}; var_dims::Vector{String} = N == 2 ? ["lon", "lat"] : ["lon", "lat", "ind"], compress::Int = 4, growable::Bool = false) where {T<:Union{AbstractFloat,Int,String},N}
 
 Save the 1D, 2D, or 3D data as netcdf file, given
 - `file` Path to save the dataset
 - `var_name` Variable name for the data in the NC file
 - `var_data` Data to save
 - `var_attribute` Variable attributes for the data, such as unit and long name
+- `var_dims` Dimension name of each dimension of the variable data
 - `compress` Compression level fro NetCDF, default is 4
 - `growable` If true, make index growable, default is false
 
 Note that this is a wrapper function of create_nc and append_nc:
 - If var_data is 1D, the dim is set to ind
-- If var_data is 2D, the dims are set to lon and lat
-- If var_data is 3D, the dims are set to long, lat, and ind
+- If var_data is 2D, and no var_dims are given, the dims are set to lon and lat
+- If var_data is 3D, and no var_dims are given, the dims are set to lon, lat, and ind
 
 #
     save_nc!(file::String, df::DataFrame, var_names::Vector{String}, var_attributes::Vector{Dict{String,String}}; compress::Int = 4, growable::Bool = false)
