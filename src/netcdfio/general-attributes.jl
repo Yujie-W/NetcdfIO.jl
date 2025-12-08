17b43aad79ba1ef48f9a06fbb9503bcdc7398081
Yujie <jesiner@gmail.com>
2025-12-08 08:32:41 +0800

    add more attribute detections

--- src/netcdfio/general-attributes.jl
+++ src/netcdfio/general-attributes.jl
@@ -16,7 +16,7 @@ Return an ordered dictionary of variable attributes, given
 function detect_attribute(varname::String, wavelength::Union{Int,Nothing} = nothing; showwarning::Bool = true)
     #
     #
-    # with exact match
+    # with exact match for dimension-related names
     #
     #
     # if the varname is latitude
@@ -43,6 +43,29 @@ function detect_attribute(varname::String, wavelength::Union{Int,Nothing} = noth
         )
     end;
 
+    #
+    #
+    # with exact match for general variables
+    #
+    #
+    # if the varname is APAR
+    if varname == "APAR"
+        return OrderedDict{String,Any}(
+            "about" => "Photosynthetically Active Radiation photons absorbed by vegetation",
+            "unit" => "μmol m⁻² s⁻¹",
+            "input_varname" => varname,
+        )
+    end;
+
+    # if the varname is Ci, PCi, or PCI
+    if varname in ["Ci", "PCi", "PCI"]
+        return OrderedDict{String,Any}(
+            "about" => "Intercellular CO₂ partial pressure",
+            "unit" => "Pa",
+            "input_varname" => varname,
+        )
+    end;
+
     # if the varname is ET
     if varname == "ET"
         return OrderedDict{String,Any}(
@@ -61,11 +84,92 @@ function detect_attribute(varname::String, wavelength::Union{Int,Nothing} = noth
         )
     end;
 
+    # if the varname is PAR
+    if uppercase(varname) in ["PAR", "PPFD"]
+        return OrderedDict{String,Any}(
+            "about" => "Photosynthetically Active Radiation photons",
+            "unit" => "μmol m⁻² s⁻¹",
+            "input_varname" => varname,
+        )
+    end;
+
+    # if the name is PPAR
+    if varname == "PPAR"
+        return OrderedDict{String,Any}(
+            "about" => "Photosynthetically Active Radiation photons that goes into the Photosystems",
+            "unit" => "μmol m⁻² s⁻¹",
+            "input_varname" => varname,
+        )
+    end;
+
     # if the varname is SIF
     if varname == "SIF" && !isnothing(wavelength)
         return OrderedDict{String,Any}(
             "about" => "Solar-Induced chlorophyll Fluorescence at $wavelength nm",
             "unit" => "W m⁻² sr⁻¹ μm⁻¹",
+            "input_varname" => varname,
+        )
+    end;
+
+    # if the varname is ΦD
+    if varname == "ΦD"
+        return OrderedDict{String,Any}(
+            "about" => "Heat dissipation Quantum Yield",
+            "unit" => "-",
+            "input_varname" => varname,
+        )
+    end;
+
+    # if the varname is ΦF
+    if varname == "ΦF"
+        return OrderedDict{String,Any}(
+            "about" => "Fluorescence Quantum Yield",
+            "unit" => "-",
+            "input_varname" => varname,
+        )
+    end;
+
+    # if the varname is ΦN
+    if varname == "ΦN"
+        return OrderedDict{String,Any}(
+            "about" => "Non-photochemical Quantum Yield",
+            "unit" => "-",
+            "input_varname" => varname,
+        )
+    end;
+
+    # if the varname is ΦP
+    if varname == "ΦP"
+        return OrderedDict{String,Any}(
+            "about" => "Photochemical Quantum Yield",
+            "unit" => "-",
+            "input_varname" => varname,
+        )
+    end;
+
+    # if the varname is ΣSIF
+    if varname == "ΣSIF"
+        return OrderedDict{String,Any}(
+            "about" => "Total Solar-Induced chlorophyll Fluorescence over all measured wavelengths that escape from the canopy",
+            "unit" => "W m⁻²",
+            "input_varname" => varname,
+        )
+    end;
+
+    # if the varname is ΣSIF_CHL
+    if varname == "ΣSIF_CHL"
+        return OrderedDict{String,Any}(
+            "about" => "Total Solar-Induced chlorophyll Fluorescence over all measured wavelengths emitted by the chlorophyll",
+            "unit" => "W m⁻²",
+            "input_varname" => varname,
+        )
+    end;
+
+    # if the varname is ΣSIF_LEAF
+    if varname == "ΣSIF_LEAF"
+        return OrderedDict{String,Any}(
+            "about" => "Total Solar-Induced chlorophyll Fluorescence over all measured wavelengths that escape from the leaf",
+            "unit" => "W m⁻²",
             "input_varname" => varname,
         )
     end;
